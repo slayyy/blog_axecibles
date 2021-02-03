b00f53e997767796b8aded8afac0eeccfb83675e
slay <decobert.a78@gmail.com>
2021-02-03 23:43:26 +0100

    send email with contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,48 +4,38 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
-use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
     #[Route('/new', name: 'contact_new', methods: ['POST'])]
-    public function new(Request $request): Response
+    public function new(Request $request, \Swift_Mailer $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($contact);
-            $entityManager->flush();
+        $data = $form->getData();
+        $name = $data->getName();
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mail = (new \Swift_Message("${name} - Contact Form"))
+                        ->setFrom($data->getEmail())
+                        ->setTo('decobert.a78@gmail.com')
+                        ->setBody(
+                            $this->renderView(
+                                'emails/contact.html.twig',
+                                ['message' => $data->getMessage()]
+                            ),
+                            'text/html'
+                        );
+
+            $mailer->send($mail);
             return $this->redirectToRoute('home');
         }
-    }
-    
-    /**
-     * @IsGranted("ROLE_ADMIN")
-     */
-    #[Route('/{id}', name: 'contact_delete', methods: ['DELETE'])]
-    public function delete(Request $request, ContactRepository $contactRepository, $id): Response
-    {
-        $contact = $contactRepository->find($id);
-
-        if($contact) {
-            if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->remove($contact);
-                $entityManager->flush();
-            }
-        }
-
-        return $this->redirectToRoute('admin_contact_index');
     }
 }
